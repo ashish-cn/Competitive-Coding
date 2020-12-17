fb658ca57bd130008b68910a057d94204fc276bb
ashish-cn <69113306+ashish-cn@users.noreply.github.com>
2020-12-17 20:57:20 +0530

    Update DFS - Cycle Detection in Directed Graph.cpp

--- DFS - Cycle Detection in Directed Graph.cpp
+++ DFS - Cycle Detection in Directed Graph.cpp
@@ -1,23 +1,25 @@
+int vis[N];
+
 bool findLoop(int v) 
 {
-	if(vis[v]==1)
+	if(vis[v] == 1)
 		return 1;
-	if(vis[v]==2)
+	if(vis[v] == 2)
 		return 0;
-	vis[v]=1;
+	vis[v] = 1;
 	for(auto &it:g[v])
 	{
 		if(findLoop(it))
 			return 1;
 	}
-	vis[v]=2;
+	vis[v] = 2;
 	return 0;
 }
 
 bool checkLoop()
 {
-	fill(vis+1, vis+n+1, 0);
-	for(int i=1;i<=n;i++)
+	fill(vis + 1, vis + n + 1, 0);
+	for(int i = 1; i <= n; i++)
 	{
 		if(!vis[i] && findLoop(i))
 			return 1;
